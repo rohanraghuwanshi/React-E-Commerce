b37fe39694c0f6641a5cfe8adf026ed16baf0fc7
rohanraghuwanshi <rohanraghuvanshi001@gmail.com>
2021-05-22 10:37:09 +0530

    feat : add header with routing

--- src/App.js
+++ src/App.js
@@ -2,24 +2,19 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage/HomePage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
-
 function App() {
   return (
-    <>
+    <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
-    </>
+    </div>
   );
 }
 
